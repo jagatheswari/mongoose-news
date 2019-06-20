d2076400a9cee7beeccdd0148149d4a15e23d358
JagatheswariManoharan <jagatheswarimanoharan@Viswanathans-MacBook-Pro.local>
2019-06-19 23:02:43 -0700

    done with frontend and server setup

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongojs = require("mongojs")
+const axios = require("axios")
+const cheerio = require("cheerio")
+const { join } = require('path')
+const app = express()
+
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+
+// Connect to the Mongo DB
+  require('mongoose').connect('mongodb://localhost/mongoose-news', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+  .then(_ => app.listen(3000))
+  // .catch(e => console.log(e))
+  // When the server starts, create and save a new User document to the db
+  // The "unique" rule in the User model's schema will prevent duplicate users from being added to the server  
+  // db.Article.create({ name: "saved articles" })
+  // .then(function(dbArticle) {
+  //   console.log(dbArticle);
+  // })
+  .catch(function(err) {
+    console.log(err.message);
+  });
+
+
+ 
+
+
